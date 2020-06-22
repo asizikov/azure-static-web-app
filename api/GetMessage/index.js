304e9ca3365acb7ff1ee55bcb28e1a0143178c64
Anton Sizikov <sizikov.anton@gmail.com>
2020-06-22 19:49:40 +0200

    return a json

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -4,12 +4,16 @@ module.exports = async function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body:{
+                text : "Hello " + (req.query.name || req.body.name)
+            } 
         };
     }
     else {
         context.res = {
-            body: "Please pass a name on the query string or in the request body"
+            body: {
+                text: "Nothing was passed in"
+            }
         };
     }
 };
